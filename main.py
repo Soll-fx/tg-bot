36fdd02f4ec995fbc2a3ddbbe390a0f0fc971f01
Soll-fx <132210459+Soll-fx@users.noreply.github.com>
2025-08-01 00:01:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я работаю 24/7 на Render!")
 
-app = ApplicationBuilder().token("8224724997:AAE896Cj7cFM5Hy0H1AEQBSDfv61oLd-_yY
-").build()
+app = ApplicationBuilder().token("8224724997:AAE896Cj7cFM5Hy0H1AEQBSDfv61oLd-_yY").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
